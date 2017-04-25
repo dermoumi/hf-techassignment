9c63f33719b83b2f4d4fc3e987f2801506e09cce
Dermoumi S <sdermoumi@gmail.com>
2017-04-25 12:03:16 +0100

    Setup initial views, although unimplmented

--- loginapp/mainapp/urls.py
+++ loginapp/mainapp/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'mainapp'
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^login', views.login, name='login'),
+    url(r'^email-notice', views.email_notice, name='email_notice'),
+    url(r'^confirm-email', views.confirm_email, name='confirm_email'),
+    url(r'^panel/manage', views.panel_manage, name='panel_manage'),
+    url(r'^panel/logout', views.panel_logout, name='panel_logout'),
 ]
--- loginapp/mainapp/views.py
+++ loginapp/mainapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+# Index view, right now all it does is redirect to user panel
+def index(request):
+    return redirect('mainapp:panel_manage')
+
+def login(request):
+    # TODO: Implement login page
+    pass
+
+def email_notice(request):
+    # TODO: Implement email notice
+    pass
+
+def confirm_email(request):
+    # TODO: Implement email confirmation
+    pass
+
+def panel_manage(request):
+    # TODO: Impelement panel manage
+    pass
+
+def panel_logout(request):
+    # TODO: Implement panel logout
+    pass
