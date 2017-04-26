a8604dda56c8f3a778107793a5e3f3abef095e3d
Dermoumi S <sdermoumi@gmail.com>
2017-04-26 09:43:57 +0100

    Fixed tests

--- loginapp/mainapp/tests.py
+++ loginapp/mainapp/tests.py
@@ -22,8 +22,8 @@ class LoginTests(StaticLiveServerTestCase):
 
     def test_empty_form(self):
         driver = self.selenium
-        driver.get('%s/signup' % self.live_server_url)
-        driver.find_element_by_link_text(u"Log in »").click()
+        driver.get('http://web:8000/signup')
+        driver.find_element_by_xpath("/html/body/div/div/p/a").click()
         driver.find_element_by_xpath("//button[@type='submit']").click()
         self.assertTrue(self.is_element_present(By.XPATH, "/html/body/div/div/form/div[1]/div/div[2]/ul/li"))
         self.assertTrue(self.is_element_present(By.XPATH, "/html/body/div/div/form/div[2]/div/div[2]/ul/li"))
--- loginapp/run_pytest.sh
+++ loginapp/run_pytest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# wait for other servers to start
-sleep 10
+# wait for other services to start
+sleep 3
 
 # Run celery worker with log level set to DEBUG
 su -m myuser -c "pytest"
